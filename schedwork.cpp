d381fd7c0ecff3c499ef1f6f19ba965404cce768
Rafael Miranda <rafaelmi@usc.edu>
2025-04-25 13:55:16 -0700

    Finished

--- schedwork.cpp
+++ schedwork.cpp
@@ -24,7 +24,68 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add your implementation of schedule() and other helper functions here
+bool isValid(const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, const AvailabilityMatrix& avail) {
+    if (sched.size() != avail.size()) { //Ensure that enough days have been counted
+        return false;
+    }
+    size_t numWorkers = avail[0].size();
+    std::vector<int> numShifts; //Counts how many times a worker has been worked (as to not exceed limit)
+    for (size_t i = 0; i < numWorkers; i++) {
+        numShifts.push_back(0);
+    }
+    for (size_t day = 0; day < sched.size(); day++) {
+        if (sched[day].size() != dailyNeed) { //Ensure that each day has enough for the day
+            return false;
+        }
+        std::set<int> avoidDuplicates;
+        for (size_t i = 0; i < sched[day].size(); i++) {
+            int workerAssigned = sched[day][i];
+            if (avail[day][workerAssigned] == 0) { //Worker availability check
+                return false;
+            }
+            if (avoidDuplicates.find(workerAssigned) != avoidDuplicates.end()) { //Check for accidentally putting worker on the same day
+                return false;
+            }
+            avoidDuplicates.insert(workerAssigned);
+            numShifts[workerAssigned] = numShifts[workerAssigned] + 1;
+            if (numShifts[workerAssigned] > maxShifts) { //Ensure no worker exceeds their max shifts
+                return false;
+            }
+        }
+    }
+    return true;
+}
+bool backtrack(size_t day, size_t space, const AvailabilityMatrix& avail, size_t dailyNeed, size_t maxShifts, DailySchedule& sched, size_t numDays, size_t numWorkers, std::vector<int>& totalShifts) {
+    if (day == numDays) {
+        return true;
+    }
+    if (space == dailyNeed) {
+        return backtrack(day + 1, 0, avail, dailyNeed, maxShifts, sched, numDays, numWorkers, totalShifts); //Move forward a day
+    }
+    for (size_t worker = 0; worker < numWorkers; worker++) {
+        Worker_T currWorker = static_cast<Worker_T>(worker);
+        if (avail[day][currWorker] == 0) { //Checks for availability
+            continue;
+        }
+        auto it = std::find(sched[day].begin(), sched[day].end(), currWorker); //Ensures no duplicates
+        if (it != sched[day].end()) {
+            continue;
+        }
+        if (totalShifts[currWorker] >= static_cast<int>(maxShifts)) { //Ensures that the worker doesn't exceed their shift limit
+            continue;
+        }
+        sched[day][space] = currWorker;
+        totalShifts[currWorker]++;
+        if (backtrack(day, space + 1, avail, dailyNeed, maxShifts, sched, numDays, numWorkers, totalShifts) == true) {
+            return true;
+        }
 
+        //Backtracking variables
+        sched[day][space] = -1;
+        totalShifts[currWorker]--;
+    }
+    return false;
+}
 bool schedule(
     const AvailabilityMatrix& avail,
     const size_t dailyNeed,
@@ -32,14 +93,24 @@ bool schedule(
     DailySchedule& sched
 )
 {
-    if(avail.size() == 0U){
+    if (avail.size() == 0U) {
         return false;
     }
     sched.clear();
     // Add your code below
-
-
-
-
+    size_t numDays = avail.size();
+    size_t numWorkers = avail[0].size();
+    for (size_t day = 0; day < numDays; day++) { //Initializes sched with an unassigned value (-1)
+        std::vector<Worker_T> row;
+        for (size_t i = 0; i < dailyNeed; i++) {
+            row.push_back(-1);
+        }
+        sched.push_back(row);
+    }
+    std::vector<int> totalShifts;
+    for (int i = 0; i < numWorkers; i++) {
+        totalShifts.push_back(0);
+    }
+    return backtrack(0, 0, avail, dailyNeed, maxShifts, sched, numDays, numWorkers, totalShifts);
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -5,6 +5,7 @@
 #include <algorithm> 
 #include <map>
 #include <set>
+#include <vector>
 #endif
 
 #include "wordle.h"
@@ -13,7 +14,72 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+/*
+bool isValid(std::vector<char> requiredLetters, std::map<int, char> keyWord, std::string entryWord, int correctSize) {
+    std::map<int, char>::iterator it;
+    if (entryWord.length() != correctSize) { //Check if length is the same
+        return false;
+    }
+    for (it = keyWord.begin(); it != keyWord.end(); it++) { //Check if entryWord has letters in correct placement
+        int idx = it->first;
+        char correctChar = it->second;
+        if (entryWord[idx] != correctChar) {
+            return false;
+        }
+    }
+    std::vector<bool> marked;
+    for (size_t i = 0; i < entryWord.length(); i++) {
+        marked.push_back(false);
+    }
+    for (size_t j = 0; j < requiredLetters.size(); j++) { //Ensures required letters are present
+        char currChar = requiredLetters[j];
+        bool found = false;
+        for (size_t i = 0; i < entryWord.length(); i++) {
+            if (entryWord[i] == currChar && marked[i] != true) {
+                found = true;
+                marked[i] = true;
+                break;
+            }
+        }
+        if (found == false) {
+            return false;
+        }
+    }
+    return true;
+}*/
+void recursiveWordle(std::string currWord, const std::string& in, std::vector<char>& requiredLetters, const std::set<std::string>& dict, std::set<std::string>& output, int idx) {
+    if (requiredLetters.size() > (in.size() - idx)) {
+        return;
+    }
 
+    if (idx == in.size()) {
+        if (currWord.length() == in.length() && dict.find(currWord) != dict.end() && requiredLetters.empty()) {
+            output.insert(currWord);
+        }
+        return;
+    }
+    if (in[idx] != '-') { //If the in[idx] is equal to a valid letter, add that letter to currWord
+        currWord.push_back(in[idx]);
+        recursiveWordle(currWord, in, requiredLetters, dict, output, idx + 1);
+        currWord.pop_back();
+    }
+    else {
+        for (char currChar = 'a'; currChar <= 'z'; currChar++) { //Iterate through alphabet
+            bool isPresent = false;
+            auto it = std::find(requiredLetters.begin(), requiredLetters.end(), currChar);
+            if (it != requiredLetters.end()) {
+                requiredLetters.erase(it);
+                isPresent = true;
+            }
+            currWord.push_back(currChar);
+            recursiveWordle(currWord, in, requiredLetters, dict, output, idx + 1);
+            currWord.pop_back();
+            if (isPresent == true) {
+                requiredLetters.insert(it, currChar);
+            }
+        }
+    }
+}
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +88,31 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    std::set<std::string> output;
+    std::vector<char> requiredLetters; //Keeps track of what letters are required
+    for (size_t i = 0; i < floating.length(); i++) {
+        requiredLetters.push_back(floating[i]);
+    }
+    recursiveWordle("", in, requiredLetters, dict, output, 0);
+    return output;
 
+    //Disregard everything below here. I found out much later in my process that I was limited in how I solved this/I didn't read the instructions carefully enough. Though it would've worked this way too
+    /*
+    std::map<int, char> keyWord; //Puts in the location of the non-dash character in first and the actual character in second
+    for (size_t i = 0; i < in.length(); i++) {
+        if(in[i] != '-') {
+            keyWord[i] = in[i];
+        }
+    }
+    int correctSize = in.length();
+    std::set<std::string>::iterator it;
+    for (it = dict.begin(); it != dict.end(); it++) {
+        std::string currWord = *it;
+        if (isValid(requiredLetters, keyWord, currWord, correctSize) == true) {
+            output.insert(currWord);
+        }
+    }
+    return output;*/
 }
 
 // Define any helper functions here
